f7f6c0e811749edaf40ba7b07d27b31f4fee0666
Louis Williams <louawilliams@outlook.com>
2020-04-05 19:24:37 -0400

    Added HomeComponent.js

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+
+function Home(props) {
+    return (
+        <div className="container">
+            <div className="row">
+                This is a test!
+            </div>
+        </div>
+    );
+}
+
+export default Home;   
