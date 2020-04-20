066a2e60e4a7b54bf4720da36290dfcaf9b65f59
Sean Moulton <s.moulton772@gmail.com>
2020-04-20 12:20:43 -0400

    removed unused functions

--- src/Functions/AboutComponent.js
+++ src/Functions/AboutComponent.js
@@ -1,101 +1,98 @@
-import React from 'react';
-import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  Card,
+  CardBody,
+  CardHeader,
+  Media,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
 //line 12 is the method how to call a created function
 
 function About(props) {
-
-    const partners = props.partners.map(partner => {
-        return (
-            <Media key={partner.id} tag="li">
-                <RenderPartner partner={partner}/>
-            </Media>
-            
-        );
-    });
-
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>About Us</BreadcrumbItem>
-                    </Breadcrumb>
-                    <h2>About Us</h2>
-                    <hr />
-                </div>
-            </div>
-            <div className="row row-content">
-                <div className="col-sm-6">
-                    <h3>Our Mission</h3>
-                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</p>
-                </div>
-                <div className="col-sm-6">
-                    <Card>
-                        <CardHeader className="bg-primary text-white"><h3>Facts At a Glance</h3></CardHeader>
-                        <CardBody>
-                            <dl className="row">
-                                <dt className="col-6">Founded</dt>
-                                <dd className="col-6">February 3, 2016</dd>
-                                <dt className="col-6">No. of Campsites in 2019</dt>
-                                <dd className="col-6">563</dd>
-                                <dt className="col-6">No. of Reviews in 2019</dt>
-                                <dd className="col-6">4388</dd>
-                                <dt className="col-6">Employees</dt>
-                                <dd className="col-6">42</dd>
-                            </dl>
-                        </CardBody>
-                    </Card>
-                </div>
-                <div className="col">
-                    <Card className="bg-light mt-3">
-                        <CardBody>
-                            <blockquote className="blockquote">
-                                <p className="mb-0">I will not follow where the path may lead, but I will go where there is no path, and I will leave a trail.</p>
-                                <footer className="blockquote-footer">Muriel Strode,{' '}
-                                    <cite title="Source Title">"Wind-Wafted Wild Flowers" -
-                                    The Open Court, 1903</cite>
-                                </footer>
-                            </blockquote>
-                        </CardBody>
-                    </Card>
-                </div>
-            </div>
-            <div className="row row-content">
-                <div className="col-12">
-                    <h3>Community Partners</h3>
-                </div>
-                <div className="col mt-4">
-                    <Media list>
-                        {partners}
-                    </Media>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/home">Home</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>About Us</BreadcrumbItem>
+          </Breadcrumb>
+          <h2>About Us</h2>
+          <hr />
         </div>
-    );
-
+      </div>
+      <div className="row row-content">
+        <div className="col-sm-6">
+          <h3>Our Mission</h3>
+          <p>
+            At vero eos et accusamus et iusto odio dignissimos ducimus qui
+            blanditiis praesentium voluptatum deleniti atque corrupti quos
+            dolores et quas molestias excepturi sint occaecati cupiditate non
+            provident, similique sunt in culpa qui officia deserunt mollitia
+            animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis
+            est et expedita distinctio. Nam libero tempore, cum soluta nobis est
+            eligendi optio cumque nihil impedit quo minus id quod maxime placeat
+            facere possimus, omnis voluptas assumenda est, omnis dolor
+            repellendus. Temporibus autem quibusdam et aut officiis debitis aut
+            rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint
+            et molestiae non recusandae. Itaque earum rerum hic tenetur a
+            sapiente delectus, ut aut reiciendis voluptatibus maiores alias
+            consequatur aut perferendis doloribus asperiores repellat."
+          </p>
+        </div>
+        <div className="col-sm-6">
+          <Card>
+            <CardHeader className="bg-primary text-white">
+              <h3>Facts At a Glance</h3>
+            </CardHeader>
+            <CardBody>
+              <dl className="row">
+                <dt className="col-6">Founded</dt>
+                <dd className="col-6">February 3, 2016</dd>
+                <dt className="col-6">No. of Campsites in 2019</dt>
+                <dd className="col-6">563</dd>
+                <dt className="col-6">No. of Reviews in 2019</dt>
+                <dd className="col-6">4388</dd>
+                <dt className="col-6">Employees</dt>
+                <dd className="col-6">42</dd>
+              </dl>
+            </CardBody>
+          </Card>
+        </div>
+        <div className="col">
+          <Card className="bg-light mt-3">
+            <CardBody>
+              <blockquote className="blockquote">
+                <p className="mb-0">
+                  I will not follow where the path may lead, but I will go where
+                  there is no path, and I will leave a trail.
+                </p>
+                <footer className="blockquote-footer">
+                  Muriel Strode,{" "}
+                  <cite title="Source Title">
+                    "Wind-Wafted Wild Flowers" - The Open Court, 1903
+                  </cite>
+                </footer>
+              </blockquote>
+            </CardBody>
+          </Card>
+        </div>
+      </div>
+      <div className="row row-content">
+        <div className="col-12">
+          <h3>Community Partners</h3>
+        </div>
+        <div className="col mt-4">
+          <Media list></Media>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-function RenderPartner({partner}){
-    
-    if (partner) {
-        return (
-            <React.Fragment>
-                <Media object src={partner.image} alt={partner.name} width="150"/>
-                <Media body className="ml-5 mb-4">
-        <Media heading>{partner.name}</Media>
-        <div>{partner.description}</div>
-                </Media>
-            </React.Fragment>
-        )
-    }
-    return <div />
-}
-
-
-
-
 
 export default About;
