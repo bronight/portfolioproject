5f6fcf2d5401e0f6796d131fee373cbb7c420248
Emmanuel Vayleux <MannyV@users.noreply.github.com>
2020-04-24 13:41:18 -0400

    Made minimal todo-list

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import Home from './HomeComponent';
+
 import TheCalendar from './CalendarComponent';
+import TodoList from './TodoComponent';
+
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+
+import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import TeamPage from './TeamComponent';
-
 
 import { fetchEvents } from '../redux/ActionCreators';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
@@ -43,6 +46,7 @@ class Main extends Component {
                     <Switch>
                         <Route path='/home' component={HomePage} />
                         <Route exact path='/calendar' render={() => <TheCalendar events={this.props.events.events} /> } />
+                        <Route exact path='/todolist' component={TodoList}/>
                         <Route exact path='/about' component={About}/>
                         <Route exact path='/contact' component={Contact}/>
                         <Route exact path='/team' component={TeamPage}/>
